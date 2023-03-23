ee69e8353907df5efbd9b9a9441481d2dde24e80
BikashMohan <53018314+BikashMohan@users.noreply.github.com>
2023-03-23 19:33:23 +0530

    Modified the interfaces (#1)

--- CrudOperations.Interfaces/IContactService.cs
+++ CrudOperations.Interfaces/IContactService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrudOperations.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace CrudOperations.Interfaces
 {
-    public interface IContactService : IAddContact, IUpdateContact, IGetContacts, IDeleteContact, IGetContact
+    public interface IContactService
     {
+        Task AddContact(Contact contact);
+        Task DeleteContact(Guid id);
+        Task<Contact?> GetContact(Guid id);
+        Task<List<Contact>> GetContactDetails();
+        Task UpdateContact(Guid id, Contact contact);
     }
 }
