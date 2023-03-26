290ecd98c371f5d18c026ee7894ce115edee7776
Bikash Mohan <bikashmohan230@gmail.com>
2023-03-26 20:39:25 +0530

    Added validation

--- CrudOperations.Models/Contact.cs
+++ CrudOperations.Models/Contact.cs
@@ -1,11 +1,21 @@
-﻿namespace CrudOperations.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperations.Models
 {
     public class Contact
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(60)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Email { get; set; }
+        [Required]
+        [StringLength(10)]
         public string Phone { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Address { get; set; }
     }
 }
